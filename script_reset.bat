55dad24ceb22a89250ff3e4b8036f7dc3cb0fced
snowdence <snowdence2911@gmail.com>
2021-12-24 00:29:36 +0700

    Change script reset

--- script_reset.bat
+++ script_reset.bat
@@ -1,4 +1,6 @@
 git fetch --all
 git reset --hard origin/main
+rm -rf mongodb/mongodb
+mkdir mongodb/mongodb
 docker-compose down --rmi=all
-docker-compose -f docker-compose.prod.yml up --build
+docker-compose -f docker-compose.yml up --build
